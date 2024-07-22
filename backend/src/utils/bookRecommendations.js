f18fb448ca3ef25622a9b22c7436699f877e1ee3
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-07-22 00:26:54 -0600

    Create file structure for backend and initialize Typescript project

---
+++ backend/src/utils/bookRecommendations.js
@@ -0,0 +1,5 @@
+/**
+ * This file could contain a function that takes a list of books (or their topics) as input and returns a list of recommended books. 
+ * This function could use a machine learning model trained on a dataset of books and their topics. 
+ * You could use the Open Library API to retrieve information about the recommended books.
+ */
---
+++ backend/src/utils/musicRecommendations.js
@@ -0,0 +1,6 @@
+/**
+ * This file could contain a function that takes a book (or its topic) as input and returns a list of recommended songs or playlists. 
+ * This function could use a machine learning model trained on a dataset of songs or playlists and their associated books or topics. 
+ * You could use a music API, such as the Spotify API, to retrieve information about the recommended songs or playlists.
+
+ */
