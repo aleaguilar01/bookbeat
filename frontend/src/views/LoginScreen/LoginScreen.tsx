97ceb87aa1bade69d77c593e0b011e38e3b3e4ce
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-08-27 11:59:25 -0400

    resolve conflicts

--- frontend/src/views/LoginScreen/LoginScreen.tsx
+++ frontend/src/views/LoginScreen/LoginScreen.tsx
@@ -1,38 +1,24 @@
-import { Button, Image } from "antd";
-import { useAuth } from "../../context/auth-context";
-
+ import { useAuth } from "../../context/auth-context";
+import { useNavigate } from "react-router";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
-const logoUrl = new URL("../../../logo.jpeg", import.meta.url).href;
-
-const SPOTIFY_CLIENT_ID: string = import.meta.env.VITE_SPOTIFY_CLIENT_ID || "";
-const REDIRECT_URI: string = "http://localhost:5173/login";
-const AUTH_URL: string = "https://accounts.spotify.com/authorize";
-const SCOPE: string =
-  "user-read-private user-read-email playlist-read-private playlist-modify-public playlist-modify-private";
-
 const LoginScreen = () => {
   const { login } = useAuth();
+  const navigate = useNavigate();
 
-  const [searchParams, setSearchParams] = useSearchParams()
-  useEffect(() => {
-    setSearchParams(window.location.hash);
-  }, []);
+  let [searchParams, setSearchParams] = useSearchParams()
+
   
-  useEffect(() => {
-    if (searchParams.get('code')) {
-      login(searchParams.get('code'));
-    }
-  }, [searchParams]);
+// Handle the login route. 
+// Redirects the user to Spotify's authorization page with required scopes.
+// const SPOTIFY_CLIENT_ID: string = process.env.SPOTIFY_CLIENT_ID || '';
+const SPOTIFY_CLIENT_ID: string = "b6ffe68f5c8649f38b7a7da32c0fb225" || '';
 
-  const params = {
-    client_id: SPOTIFY_CLIENT_ID,
-    response_type: "code",
-    scope: SCOPE,
-    redirect_uri: REDIRECT_URI,
-    show_dialog: true, // set to true for testing
-  };
+// const REDIRECT_URI: string = 'http://localhost:3000/music/callback';
+const REDIRECT_URI: string = 'http://localhost:5173/login';
 
 const AUTH_URL: string = 'https://accounts.spotify.com/authorize';
 
@@ -73,15 +59,8 @@ useEffect(() => {
 
   return (
     <div>
-      <Image src={logoUrl} alt="logo" style={{ height: 300 }} />
-      <Button
-        style={{ backgroundColor: "purple", color: "white" }}
-        href={auth_url}
-        shape="round"
-        size="large"
-      >
-        Login With Spotify
-      </Button>
+     <a href={auth_url} >Login With Spotify</a>
+      
     </div>
   );
 };
